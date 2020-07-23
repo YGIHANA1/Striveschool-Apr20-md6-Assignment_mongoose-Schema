64c4a32ee91ab85af562d7d88d7507540a6148b3
Yannick.Gi <61831175+YGIHANA1@users.noreply.github.com>
2020-07-23 20:07:04 +0200

    CRUD connected to mongodb- mongoose

--- src/models/names/index.js
+++ src/models/names/index.js
@@ -1,13 +1,48 @@
 const express=require("express")
 const nameSchema =require("./schema")
 const namesRouter=express.Router()
-namesRouter.post("/", async (req,res)=>{
-    const Name=new nameSchema(req.body)
-     const name= await  Name.save()
+namesRouter.post("/", async (req,res,next)=>{
+ try{   const Name=new nameSchema(req.body)
+     const name= await Name.save()
     res.send(name)
+ }catch(error){
+     next(error)
+ }
 })
 
+namesRouter.get("/", async(req,res,next)=>{
+ try{   const names= await nameSchema.find(req.query)
+    res.send(names)
+}catch (error)
+{
+ next(error)
+}
+})
 
-
+namesRouter.get("/:id",async(req,res,next)=>{
+    try{
+const _id= req.params.id
+const name= await nameSchema.findById(_id)
+res.send(name)
+    }catch(error){
+        next(error)
+    }
+})
+namesRouter.put("/:id",async(req,res,next)=>{
+    try{
+  const NAME= await nameSchema.findByIdAndUpdate(req.params.id, req.body) 
+  res.send(NAME)     
+    }catch(error){
+     next(error)
+    }
+})
+namesRouter.delete("/:id",async(req,res,next)=>{
+    try{
+const nameDeleted= await nameScheme.findByIdAndDelete(req.params.id)  
+res.send(nameDeleted)      
+    }catch(error){
+        next(error)
+    }
+})
 
 module.exports= namesRouter
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express= require("express")
 const namesRouter= require("./models/names")
+const mongoose=require("mongoose")
 const dotenv=require("dotenv")
 dotenv.config()
 const server= express()
@@ -8,5 +9,10 @@ const port=process.env.PORT
 server.use(express.json())
 server.use("/names",namesRouter)
 
+mongoose
+.connect("mongodb://localhost:27017/names",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 
 server.listen(process.env.PORT || 3002 ,()=>console.log("server is running on port",port))
